88b0f615c742efc62baaff0a24dcddcd9531c89a
tostos <saittosun2@gmail.com>
2020-04-12 08:57:53 +0200

    fifth step

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -22,7 +22,10 @@ class Courses extends Component {
                             return (
                                 <Link 
                                     key={course.id} 
-                                    to={this.props.match.url + '/' + course.id}>
+                                    to={{
+                                        pathname: this.props.match.url + '/' + course.id,
+                                        search: '?title=' + course.title
+                                    }}>
                                     <article 
                                         className="Course">
                                         {course.title}
